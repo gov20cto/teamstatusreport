69c5bbf57f8a413242a7b911f2f49272bf221639
Javier Muniz <javier@granicus.com>
2011-06-10 14:28:03 -0700

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nickninja::Application.config.secret_token = '051fbbeae086d1f51d0edbf304b6a40db8decd96848ea21f3e835f5237d52d3a22b1665035b1b2d34007bfa2f3fe5a1721d3d5d02b965af861a8486f6e5f6151'
