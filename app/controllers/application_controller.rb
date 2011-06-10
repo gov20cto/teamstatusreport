d2bfe2740db2b0c5909aae84d479e1c7dc7591e3
Javier Muniz <javier@granicus.com>
2011-06-10 15:36:35 -0700

    first rev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  @api_key = 'c17bafe18ce469e3a4300873de284dc24e3fcb78'
   protect_from_forgery
 end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,28 @@
+class HomeController < ApplicationController
+  def index
+    @projects = ScrumNinja.projects('c17bafe18ce469e3a4300873de284dc24e3fcb78')
+    @projects.each do |project|
+      project.stories = ScrumNinja.project_stories('c17bafe18ce469e3a4300873de284dc24e3fcb78',project.id)
+      project.card_wall = ScrumNinja.project_card_wall('c17bafe18ce469e3a4300873de284dc24e3fcb78',project.id)
+      project.stories.each do |story|
+        story.tasks = [];
+        if not project.card_wall.nil? then
+          project.card_wall.each do |task|
+            if(task.story_id == story.id) then
+              story.tasks << task
+            end
+          end
+        end
+        total = 0;
+        completed = 0;
+        story.tasks.each do |task|
+          total = total + 1
+          completed = completed + 1 if task.status == "done"
+        end
+        story.total_tasks = total;
+        story.completed_tasks = completed;
+        story.completed_percent = (total == 0) ? 0 : ((completed.to_f / total.to_f) * 100).floor
+      end
+    end
+  end
+end
