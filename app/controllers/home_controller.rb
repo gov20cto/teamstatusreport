d34043ec219bde8e888f522ebe302713d92e9606
Javier Muniz <javier@granicus.com>
2011-06-21 13:59:12 -0700

    i hate it when i'm an idiot

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,13 @@ class HomeController < ApplicationController
   before_filter :require_allowed_ip!
   
   def index
-    @projects = @scrumninja.projects
+    @projects = @scrumninja.projects.find_all { |p| @project_map.keys.include? p.name.downcase } 
     @projects.each do |project|
-      next unless @project_map.keys.include? project.name.downcase
       project.estimates = []
       project.stories = @scrumninja.project_stories project.id
        
       project.stories = [] if project.stories.nil?
       project.burndown = @scrumninja.project_burndown project.id
-      project.burndown = Hashie::Mash.new if project.burndown.nil?
-      project.burndown.estimates = [] if project.burndown.estimates.nil?
     end
   end
   
